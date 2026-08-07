5975024aa7c2d9a4d63ee779b11b66627b0ea2b1
buatapps <buatappsbuatapps@gmail.com>
2026-08-07 23:28:10 +0700

    feat: redesign stiker kendaraan print layout with QR box and cut marker

--- app/Views/general_affairs/print.php
+++ app/Views/general_affairs/print.php
@@ -45,7 +45,6 @@
             width: 79mm;
             height: 38.5mm;
             border: 1mm solid #000;
-            border-radius: 2mm;
             overflow: hidden;
             padding-left: 2px;
             margin-top: 1mm;
@@ -57,14 +56,78 @@
             page-break-after: auto;
         }
 
+        .vertical-divider {
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 50%;
+            border-left: 0.4mm solid #000;
+        }
+
         .label-left {
             position: absolute;
             top: 3mm;
             bottom: 3mm;
             left: calc(3mm + 2px);
-            right: 40mm;
+            right: calc(50% + 0.5mm);
             display: flex;
             flex-direction: column;
+        }
+
+        .label-right {
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 50%;
+            right: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        .qr-box {
+            margin: auto 1.5mm;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            background: #fff;
+            border-radius: 2mm;
+            padding: 0.6mm 1.5mm;
+        }
+
+        .label-right .namicoh {
+            font-size: 9pt;
+            letter-spacing: 1.5pt;
+            color: #000;
+            margin-top: 0.6mm;
+        }
+
+        .label-right .always-safe,
+        .label-right .safety-riding {
+            font-size: 6.5pt;
+            margin-top: 0.5mm;
+            margin-bottom: -0.3mm;
+            line-height: 1;
+            color: #000;
+        }
+
+        .qr-img {
+            margin-top: 1mm;
+            width: 20mm;
+            height: 20mm;
+            border: 0.3mm solid #000;
+            padding: 0.5mm;
+            background: #fff;
+        }
+
+        .label-right .plate-band {
+            width: 100%;
+            margin-top: 0;
+            font-size: 8pt;
+            letter-spacing: 1.5pt;
+            padding: 0.6mm 0;
+            background: transparent;
+            color: #000;
         }
 
         .namicoh {
@@ -108,22 +171,14 @@
         }
 
         .plate-band {
-            background: #000;
-            color: #fff;
+            background: #fff;
+            color: #000;
             font-size: 10pt;
             font-weight: bold;
             letter-spacing: 2pt;
             text-align: center;
             padding: 1.2mm 0;
-            border-radius: 1mm;
-        }
-
-        .qr-img {
-            position: absolute;
-            top: calc(1mm + 5px);
-            right: calc(1mm + 5px);
-            width: 33mm;
-            height: 33mm;
+            border-radius: 0;
         }
     </style>
 </head>
@@ -135,6 +190,7 @@
 
     <?php foreach ($employees as $emp): ?>
         <div class="label">
+            <div class="vertical-divider"></div>
             <div class="label-left">
                 <div class="namicoh">Namicoh</div>
                 <div class="safety-riding">Safety Riding</div>
@@ -147,17 +203,24 @@
                 <div class="info-row"><span class="lbl">SIM Berlaku : </span><span class="val"><?= esc($emp['sim_masa_berlaku']); ?></span></div>
                 <div class="divider"></div>
                 <div class="spacer"></div>
-                <div class="plate-band"><?= esc($emp['nomor_plat']); ?></div>
             </div>
-            <img src="<?= render_qrcode(json_encode([
-                'employee_id'        => $emp['employee_id'],
-                'name'               => $emp['name'],
-                'division'           => $emp['division'],
-                'nomor_plat'         => $emp['nomor_plat'],
-                'masa_berlaku_pajak' => $emp['masa_berlaku_pajak'],
-                'masa_berlaku_plat'  => $emp['masa_berlaku_plat'],
-                'sim_masa_berlaku'   => $emp['sim_masa_berlaku'],
-            ], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)); ?>" alt="QR Code" class="qr-img" />
+            <div class="label-right">
+                <div class="qr-box">
+                    <div class="namicoh">Namicoh</div>
+                    <div class="always-safe">Always Safe</div>
+                    <img src="<?= render_qrcode(json_encode([
+                        'employee_id'        => $emp['employee_id'],
+                        'name'               => $emp['name'],
+                        'division'           => $emp['division'],
+                        'nomor_plat'         => $emp['nomor_plat'],
+                        'masa_berlaku_pajak' => $emp['masa_berlaku_pajak'],
+                        'masa_berlaku_plat'  => $emp['masa_berlaku_plat'],
+                        'sim_masa_berlaku'   => $emp['sim_masa_berlaku'],
+                    ], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)); ?>" alt="QR Code" class="qr-img" />
+                    <div class="safety-riding">Safety Riding</div>
+                    <div class="plate-band"><?= esc($emp['nomor_plat']); ?></div>
+                </div>
+            </div>
         </div>
     <?php endforeach; ?>
 </body>
