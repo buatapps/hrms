59d4ad26814e82a7a0b318ef1a8a587b0ebf1653
buatapps <buatappsbuatapps@gmail.com>
2026-09-02 02:00:56 +0700

    fix: update working directory in attendance_download batch file to ensure correct script execution

--- attendance_download.bat
+++ attendance_download.bat
@@ -1,5 +1,6 @@
 @echo off
 setlocal
+cd /d C:\xampp\htdocs\hrms
 set /p EMAIL=<C:\xampp\htdocs\hrms\writable\notif_email.cfg
 C:\xampp\php\php.exe C:\xampp\htdocs\hrms\spark attendance:download --email %EMAIL%
 endlocal
