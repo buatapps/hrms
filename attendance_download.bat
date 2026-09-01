ff3cdb24e8c00cd13f3d9bf38675ffcc05792cfa
buatapps <buatappsbuatapps@gmail.com>
2026-09-02 01:51:41 +0700

    feat: pass email address from configuration file to attendance download script

--- attendance_download.bat
+++ attendance_download.bat
@@ -1,2 +1,5 @@
 @echo off
-C:\xampp\php\php.exe C:\xampp\htdocs\hrms\spark attendance:download
+setlocal
+set /p EMAIL=<C:\xampp\htdocs\hrms\writable\notif_email.cfg
+C:\xampp\php\php.exe C:\xampp\htdocs\hrms\spark attendance:download --email %EMAIL%
+endlocal
